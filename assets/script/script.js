fea59857f94984c780b5a78515ef3e37ebee9fcd
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-02 18:56:26 -0500

    added fetch function based on user input

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,28 @@
-var userInput = document.querySelector('#city-search').value
-console.log(userInput)
-var cityName = 'Austin'
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=d0077aed95867e04393883a9e7a5d3f5')
-.then(response => response.json()).then(data => console.log(data))
+//variables
+var citySearchEl = document.querySelector('#city-search')
+var submitButton = document.querySelector("#button")
+var userSearch = ""
+var apiKey = "d0077aed95867e04393883a9e7a5d3f5"
+
+function formSearch(event) {
+    var userSearch = citySearchEl.value.trim();
+    var userSearch = userSearch.toUpperCase();
+    console.log(userSearch)
+    fetchCurrentWeather(userSearch)
+}
+
+function fetchCurrentWeather(city){
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    fetch(url).then(response => response.json()).then(data => console.log(data))
+
+}
+
+fetchCurrentWeather();
+
+// event listeners
+$(document).ready(function() {
+    $("button").click(function(event) {
+        formSearch();
+        event.preventDefault();
+    });
+});
