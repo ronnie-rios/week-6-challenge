7a5f8931aa85b064bfbdd0b9cd52e02cf7f39c89
Ronnie Rios <ronniesmail14@gmail.com>
2021-05-02 20:59:04 -0500

    render current weather to div

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,7 @@
 //variables
 var citySearchEl = document.querySelector('#city-search')
 var submitButton = document.querySelector("#button")
+var weatherEl = document.querySelector("#weather-display")
 var userSearch = ""
 var apiKey = "d0077aed95867e04393883a9e7a5d3f5"
 
@@ -13,10 +14,37 @@ function formSearch(event) {
 
 function fetchCurrentWeather(city){
     let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
-    fetch(url).then(response => response.json()).then(data => console.log(data))
+    fetch(url).then(function(response){
+        //check if a city was entered
+        if (response.ok) {
+            response.json().then(function(data){
+                console.log(data)
+                createCurrentWeather(data)
+
+            })
+        } else {
+            alert("Plese enter a city.")
+        }
+    }) 
 
 }
+function createCurrentWeather(info) {
+    //create elements
+    const currentTemp = document.createElement('h3')
+    const currentHumidity = document.createElement('p')
+    const currentWind = document.createElement('p')
+    const currentUv = document.createElement('p')
+    //grab data and inserting to elements
+    currentTemp.innerHTML = "Temperature: " + info.main.temp
+    currentHumidity.innerHTML = "Humidity: " + info.main.humidity
+    currentWind.innerHTML = "Wind Speed is " + info.wind.speed
+    
+    //append to weather display
+    weatherEl.appendChild(currentTemp)
+    weatherEl.appendChild(currentHumidity)
+    weatherEl.appendChild(currentWind)
 
+}
 fetchCurrentWeather();
 
 // event listeners
